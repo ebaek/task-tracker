de5ec0b5057943044f9a89834efc2eea402bc97a
Esther Baek <esther.baek5@gmail.com>
2020-09-15 13:43:16 -0400

    Add task routes for get, add, update, delete

---
+++ backend/routes/tasks.js
@@ -0,0 +1,60 @@
+const router = require('express').Router();
+let Task = require('../models/task.model');
+
+router.route('/').get( (req, res) => {
+    Task.find()
+        .then(tasks => res.json(tasks))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post( (req, res) => {
+    const { name, description } = req.body;
+    const dueDate = Date.parse(req.body.dueDate);
+
+    const newTask = new Task({
+        name,
+        description,
+        dueDate,
+    });
+
+    newTask.save()
+        .then( () => res.json('New task added'))
+        .catch( err => res.status(400).json('Error ' + err));
+
+});
+
+router.route('/:id').get( (req, res) => {
+    Task.findById(req.params.id)
+        .then( task => {
+            res.json(task)
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    Task.findByIdAndDelete(req.params.id)
+        .then( () => {
+            res.json('Task deleted')
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('update/:id').post((req, res) => {
+    Task.findById(req.params.id)
+        .then( task => {
+            const { name, description, dueDate, complete } = req.body;
+
+            task.name = name;
+            task.description = description;
+            task.dueDate = Date.parse(dueDate);
+            task.complete = complete.toString();
+
+            task.save()
+                .then(() => res.json('Task updated'))
+                .catch(err => res.status(400).json('Error ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,10 @@ connection.once('open', () => {
     console.log('Successfully connected to MongoDB database')
 })
 
+// routers
+const tasksRouter = require('./routes/tasks');
+app.use('/tasks', tasksRouter);
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 })
